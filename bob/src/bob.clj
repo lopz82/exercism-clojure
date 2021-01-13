539aca71b4c434cbe58383a635f46a8258d11fc2
lopz82 <jorge.lopez.guzman@outlook.es>
2021-01-13 11:35:40 +0100

    Adding bob exercise

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -1,25 +1,27 @@
-(ns bob)
-
-(def valid-chars (into #{} (concat (map char (concat (range 97 123) ;; uppercase letter
-                                                     (range 65 91))) ;; lowercase letters
-                                   [\? \!])))
+(ns bob
+  (:require [clojure.string :as s]))
 
 (defn valid-char? [s]
-  (contains? valid-chars s))
+  (boolean (re-matches #"[A-Za-z?!]" (str s))))
+
+(defn punctuation? [s]
+  (contains? (set "?!") s))
 
 (defn question? [s]
-  (= \? (last s)))
+  (s/ends-with? s "?"))
 
 (defn yelling? [s]
-  (if (or (empty? s) (= "?" s))
-    false
-    (= s (clojure.string/upper-case s))))
+  (let [s (s/join (filter (complement punctuation?) s))]
+    (if (s/blank? s)
+      false
+      (= s (s/upper-case s)))))
 
 (defn response-for [s]
   (let [f (apply str (filter valid-char? s))]
     (cond
-      (and (yelling? f) (question? f)) "Calm down, I know what I'm doing!"
+      (and (yelling? f)
+           (question? f)) "Calm down, I know what I'm doing!"
       (yelling? f) "Whoa, chill out!"
       (question? f) "Sure."
-      (empty? (clojure.string/trim s)) "Fine. Be that way!"
+      (empty? (s/trim s)) "Fine. Be that way!"
       :else "Whatever.")))
